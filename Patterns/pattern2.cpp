c7835c89ce1d9cf0f07725dfe74074d4157b015b
Om Patil <97589129+omhpatil@users.noreply.github.com>
2023-10-24 23:24:22 +0530

    Add files via upload

---
+++ Patterns/pattern2.cpp
@@ -0,0 +1,27 @@
+/*
+
+Input Format: N = 3
+Result: 
+* 
+* * 
+* * *
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for(int i=1;i<=3;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+        
+    }
+
+    return 0;
+}
---
+++ Patterns/pattern4.cpp
@@ -0,0 +1,30 @@
+/*
+
+Input Format: N = 6
+Result:
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+6 6 6 6 6 6
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for(int i=1;i<=6;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
+
