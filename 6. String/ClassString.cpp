c2f26eaaec538f416c34201ac06ef8a33812d6f5
Om Patil <97589129+omhpatil@users.noreply.github.com>
2023-11-10 16:14:48 +0530

    Add files via upload

---
+++ 6. String/ClassString.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+    string str;
+    cout<<"Enter string ";
+    getline(cin,str);
+
+    cout<<str;
+
+    return 0;
+
+}
